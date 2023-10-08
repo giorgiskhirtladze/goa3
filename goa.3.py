d29f3b520aa2bd56edf77b4bc01903ce5838dfb1
giorgiskhirtladze <144174679+giorgiskhirtladze@users.noreply.github.com>
2023-10-08 22:51:14 +0400

    Add files via upload

---
+++ goa.3.py
@@ -0,0 +1,8 @@
+user_num = int(input("enter eny number : "))
+
+if user_num <10000:
+    print("goa-ში სწავლობდი?")
+    elif user_num ==10000
+else:
+if user_num>1000
+    print("შემოსულიყავი goa-ში მატრიცელო")
